70020afd1129533523bd6163cd2fd18c75221355
yumingang <69136169+yumingang@users.noreply.github.com>
2020-08-15 16:10:50 -0400

    Template changes
    Installed show current template plugin and created custom templates for home page, no sidebar, and category.

---
+++ wp-content/themes/child_of_twentysixteen/no-sidebar.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * The template for displaying pages with no sidebar
+ *
+ * Template Name: no-sidebar.php
+ *
+ * @package WordPress
+ * @subpackage Twenty_Sixteen
+ * @since Twenty Sixteen 1.0
+ */
+
+get_header(); ?>
+
+<div id="primary" class="content-area">
+	<main id="main" class="site-main" role="main">
+		<?php
+		// Start the loop.
+		while ( have_posts() ) :
+			the_post();
+
+			// Include the page content template.
+			get_template_part( 'template-parts/content', 'page' );
+
+			// If comments are open or we have at least one comment, load up the comment template.
+			if ( comments_open() || get_comments_number() ) {
+				comments_template();
+			}
+
+			// End the loop.
+		endwhile;
+		?>
+
+	</main><!-- .site-main -->
+
+	<?php get_sidebar( 'content-bottom' ); ?>
+
+</div><!-- .content-area -->
+
+<?php //get_sidebar(); ?> <!--commented out sidebar-->
+<?php get_footer(); ?>
